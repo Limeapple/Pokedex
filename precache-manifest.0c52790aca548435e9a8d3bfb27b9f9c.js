ae7a47bc1c2c1217ac9bbcc5b7dc06d322d437b4
Limeapple <Kevinliu54321@gmail.com>
2019-05-05 15:28:47 -0500

    Updates

---
+++ precache-manifest.0c52790aca548435e9a8d3bfb27b9f9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b419026b7810974d5e4e",
+    "url": "/Pokedex/static/css/main.0788c74f.chunk.css"
+  },
+  {
+    "revision": "b419026b7810974d5e4e",
+    "url": "/Pokedex/static/js/main.50bfd81f.chunk.js"
+  },
+  {
+    "revision": "119fa3083960fadc0325",
+    "url": "/Pokedex/static/js/runtime~main.cf3900f6.js"
+  },
+  {
+    "revision": "d42d3864ff23e3770b2b",
+    "url": "/Pokedex/static/js/2.d43a99c2.chunk.js"
+  },
+  {
+    "revision": "29ff1e9d64e6460434afa16718e4a0a7",
+    "url": "/Pokedex/index.html"
+  }
+];
